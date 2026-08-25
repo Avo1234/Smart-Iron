36e718071b8b3bd2e3b2d0d4f85bec685eab397d
Wills Einstein <herbertasante66@gmail.com>
2026-08-25 13:51:26 +0000

    refactor: track TFT render state

--- firmware_iron/display_controller.h
+++ firmware_iron/display_controller.h
@@ -16,10 +16,16 @@ class DisplayController {
   Adafruit_ILI9341 tft_;
   XPT2046_Touchscreen touch_;
   bool wasTouched_ = false;
+  bool staticLayoutDrawn_ = false;
+  bool lastFaultState_ = false;
+  AppState lastState_;
   uint32_t lastRenderMs_ = 0;
 
-  void drawMain(const AppState& state);
+  void drawStaticLayout();
+  void drawDynamicValues(const AppState& state, bool redrawAll);
   void drawFault(const AppState& state);
+  void drawFaultDynamicValues(const AppState& state, bool redrawAll);
+  void drawPreset(uint8_t index, bool selected);
   bool readNewPress(int& x, int& y);
   static bool inside(int x, int y, int left, int top, int width, int height);
 };
